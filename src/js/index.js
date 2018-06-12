0d6ef97ef40313094fba6d4f7f7381173c2158a0
sholhov <asolohov@gmail.com>
2018-06-12 21:17:54 +0300

    Add navi and preloader

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function scrollBtnInit() {
-	let btn = document.querySelector('#move-to-top');
+	const btn = document.querySelector('#move-to-top');
 	btn.addEventListener('click', Tools.scrollPageToTop);
+}
+
+window.addEventListener("load", onWindowLoad);
+
+function onWindowLoad() {
+	const preloader = document.querySelector("#preloader");
+	preloader.classList.add("end");
 }
