242b61841b5c2976cd3429e5341f15546d206596
sholhov <asolohov@gmail.com>
2018-06-13 13:19:36 +0300

    add scroll to top button

--- src/js/index.js
+++ src/js/index.js
@@ -3,19 +3,37 @@ import * as Tools from './tools'
 document.addEventListener("DOMContentLoaded", () => {
 	scrollBtnInit();
 	document.body.style.overflow = "hidden";
+
+	const de = document.documentElement;
+	const btn = document.querySelector('#move-to-top');
+
+	/**
+	 * Показывает кнопку скрола когда страница ниже высоты экрана
+	 */
+	window.addEventListener("scroll", () => {
+		if (de.scrollTop > de.clientHeight) {
+			btn.classList.add('visible')
+		} else {
+			btn.classList.remove('visible');
+		}
+	});
 });
 
+/**
+ * Прокрутка страницы к началу
+ */
 function scrollBtnInit() {
 	const btn = document.querySelector('#move-to-top');
 	btn.addEventListener('click', Tools.scrollPageToTop);
 }
 
 window.addEventListener("load", onWindowLoad);
-
 function onWindowLoad() {
 	const preloader = document.querySelector("#preloader");
-	preloader.classList.add("end");
 	const body = document.body;
+	
+	// показ экрана загрузкич
+	preloader.classList.add("end");
 	body.style.overflow = "";
 	body.classList.add("loaded");
 }
